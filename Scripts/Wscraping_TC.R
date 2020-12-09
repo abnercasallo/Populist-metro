33e14e22a4332292d9a482a8a6ae4324d424c350
abnercasallo <68797376+abnercasallo@users.noreply.github.com>
2020-12-09 14:04:02 -0500

    función scrap del TC
    Función que baja las sentencias de inconstitucionalidad de leyes

--- Scripts/Wscraping_TC.R
+++ Scripts/Wscraping_TC.R
@@ -5,16 +5,41 @@ library(wdman)    #Navegador fantasma, permite usar rsDriver
 library(robotstxt)
 library(tidyverse)
 
-#num=c(1:15)
-num=1
-UrlMadre<-paste0("http://181.177.234.7/buscarRes/public/resolucionjur?filtro=A&search=Inconstitucional%2C+Ejecutivo&demandante=&demandado=&numexpediente=&anoingreso=&idtipoproceso=0&anopublica=&pg=",num)
-html<-read_html(UrlMadre)
 
-# paths_allowed(paths = c(UrlMadre)) # TRUE
-# options(encoding = "utf-8")
-# server<-phantomjs(port=5015L) 
+#CREAREMOS LA BASE DE DATOS DE LEYES INCONSTITUCIONALES
+base<-data.frame("LEY DEMANDADA")
+base
 
-pag_text<-html%>%
-  html_nodes(xpath='//*[@class="date"]')%>% 
-  html_text()
-pag_text
+for (i in 1:15){
+  UrlMadre<-paste0("http://181.177.234.7/buscarRes/public/resolucionjur?filtro=A&search=Inconstitucional%2C+Ley&demandante=&demandado=ley&numexpediente=&anoingreso=&idtipoproceso=0&anopublica=&pg=",i)
+  html<-read_html(UrlMadre)
+  pag_text<-html%>%
+    html_nodes(xpath='//*[@class="date"]')%>% 
+    html_text()
+  pag_text
+  for (i in 1:20) {
+    if (i %% 2== 0){    #Condición par (demandado: Leyes)
+      demandado<-pag_text[i]
+      base[ nrow(base) + 1,] <- demandado     #appendeamos
+      
+    }}
+    
+}
+
+
+  
+
+# pag_text[2] 
+# 
+# 
+# UrlMadre<-paste0("http://181.177.234.7/buscarRes/public/resolucionjur?filtro=A&search=Inconstitucional%2C+Ley&demandante=&demandado=ley&numexpediente=&anoingreso=&idtipoproceso=0&anopublica=&pg=1",num)
+# html<-read_html(UrlMadre)
+# 
+# # paths_allowed(paths = c(UrlMadre)) # TRUE
+# # options(encoding = "utf-8")
+# # server<-phantomjs(port=5015L) 
+# 
+# pag_text<-html%>%
+#   html_nodes(xpath='//*[@class="date"]')%>% 
+#   html_text()
+# pag_text
